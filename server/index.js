3f764edc08a6c763d565c1183736f30417ef05d6
davidlira1 <liradavid100@gmail.com>
2020-11-23 21:17:01 -0800

    Create server filed that opens on port 3000

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.listen(port, (err) => {
+    if(end) console.error(err);
+    console.log(`Listening on port: ${port}`);
+})
